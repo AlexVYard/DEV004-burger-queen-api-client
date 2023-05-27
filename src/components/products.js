95a97d528de58fa6fd6f64bc0f401b9e36fc3292
AlexVYard <alexvegayard@gmail.com>
2023-05-27 11:59:53 -0500

    separete components for office and workers

--- src/components/products.js
+++ src/components/products.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { /* useState,  */useEffect } from 'react'
 import { /* Navigate,  */useNavigate } from "react-router-dom"
 import './styles.css';
 // import { signIn } from '../scripts/signIn';
